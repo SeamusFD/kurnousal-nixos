02f53d13b28095961d8ee4e25e95b58a632aae58
SeamusFD <armstrongmediahome@icloud.com>
2024-03-23 17:24:00 -0400

    98 current  2024-03-23 17:23:54  24.05pre601756.44d0940ea560  6.6.22                          *

--- scripts/custom-bash-scripts.nix
+++ scripts/custom-bash-scripts.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}:
 with builtins; let
-  rebuild = pkgs.writeShellScriptBin "rebuild" (builtins.readFile "/etc/nixos/kurnousal-home/scripts/nixos-rebuild.sh");
+  rebuild = pkgs.writeShellScriptBin "rebuild" (builtins.readFile "/etc/nixos/kurnousal-nixos/scripts/nixos-rebuild.sh");
 in {
   environment.systemPackages = [
     rebuild
