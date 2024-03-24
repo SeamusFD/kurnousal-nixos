c1b9f17f7fc7bf98fae3984f0c4ca41ce5e1047e
SeamusFD <armstrongmediahome@icloud.com>
2024-03-24 12:59:14 -0400

    105 current  2024-03-24 12:59:08  24.05pre601756.44d0940ea560  6.6.22                          *

--- home-manager/waybar/waybar.nix
+++ home-manager/waybar/waybar.nix
@@ -5,5 +5,40 @@
 }: {
   programs.waybar = {
     enable = true;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        height = 30;
+        output = [
+          "HDMI-A-1"
+          "DP-3"
+        ];
+        modules-left = [];
+        modules-center = [];
+        modules-right = ["pulseaudio"];
+      };
+
+      "pulseaudio" = {
+        "format" = "{icon} {volume}";
+        "rotate" = 0;
+        "format-muted" = "婢";
+        "on-click" = "pavucontrol -t 3";
+        "on-click-middle" = "~/.config/hyprdots/scripts/volumecontrol.sh -o m";
+        "on-scroll-up" = "~/.config/hyprdots/scripts/volumecontrol.sh -o i";
+        "on-scroll-down" = "~/.config/hyprdots/scripts/volumecontrol.sh -o d";
+        "tooltip-format" = "{icon} {desc} // {volume}%";
+        "scroll-step" = 5;
+        "format-icons" = {
+          "headphone" = "";
+          "hands-free" = "";
+          "headset" = "";
+          "phone" = "";
+          "portable" = "";
+          "car" = "";
+          "default" = ["" "" ""];
+        };
+      };
+    };
   };
 }
