b781c7b55bc48129f5c2d6274ef7042b9cc4d47a
SeamusFD <armstrongmediahome@icloud.com>
2024-03-25 01:06:40 -0400

    112 current  2024-03-25 01:06:33  24.05pre601756.44d0940ea560  6.6.22                          *

--- home-manager/waybar/waybar.nix
+++ home-manager/waybar/waybar.nix
@@ -10,24 +10,24 @@ with builtins; {
       mainBar = {
         layer = "top";
         position = "top";
-        height = 15;
+        height = 30;
+        mod = "dock";
+        exclusive = true;
         output = [
           "HDMI-A-1"
           "DP-3"
         ];
-        modules-left = [];
-        modules-center = ["hyprland/workspaces"];
-        modules-right = [];
+        modules-left = ["clock"];
+        modules-center = ["wlr/workspaces"];
+        modules-right = ["temperature" "network" "bluetooth" "pulseaudio" "pulseaudio#microphone" "tray"];
       };
 
       "pulseaudio" = {
-        "format" = "{icon} {volume}";
-        "rotate" = 0;
-        "format-muted" = "婢";
-        "on-click" = "pavucontrol -t 3";
-        "on-click-middle" = "~/.config/hyprdots/scripts/volumecontrol.sh -o m";
-        "on-scroll-up" = "~/.config/hyprdots/scripts/volumecontrol.sh -o i";
-        "on-scroll-down" = "~/.config/hyprdots/scripts/volumecontrol.sh -o d";
+        "format" = "{icon} {volume}%";
+        "format-muted" = " Muted";
+        "on-click" = "pamixer -t";
+        "on-scroll-up" = "pamixer -i 5";
+        "on-scroll-down" = "pamixer -d 5";
         "tooltip-format" = "{icon} {desc} // {volume}%";
         "scroll-step" = 5;
         "format-icons" = {
@@ -40,13 +40,55 @@ with builtins; {
           "default" = ["" "" ""];
         };
       };
-      "hyprland/workspaces" = {
+      "pulseaudio#microphone" = {
+        "format" = "{format_source}";
+        "format-source" = "Mic: {volume}%";
+        "format-source-muted" = "Mic: Muted";
+        "on-click" = "pamixer --default-source -t";
+        "on-scroll-up" = "pamixer --default-source -i 5";
+        "on-scroll-down" = "pamixer --default-source -d 5";
+        "scroll-step" = 5;
+      };
+      "wlr/workspaces" = {
         "disable-scroll" = true;
         "all-outputs" = true;
-        "active-only" = false;
         "on-click" = "activate";
         "persistent-workspaces" = {
+          "1" = [];
+          "2" = [];
+          "3" = [];
+          "4" = [];
+          "5" = [];
+          "6" = [];
+          "7" = [];
+          "8" = [];
+          "9" = [];
+          "10" = [];
         };
+      };
+      "tray" = {
+        "icon-size" = 15;
+        "spacing" = 10;
+      };
+      "clock" = {
+        "format" = "{: %I:%M %p   %a, %b %e}";
+        "tooltip-format" = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+      };
+      "network" = {
+        "format-wifi" = "  {signalStrength}%";
+        "format-ethernet" = "{ipaddr}/{cidr} ";
+        "tooltip-format" = "{essid} - {ifname} via {gwaddr} ";
+        "format-linked" = "{ifname} (No IP) ";
+        "format-disconnected" = "Disconnected ⚠";
+        "format-alt" = "{ifname}:{essid} {ipaddr}/{cidr}";
+      };
+      "bluetooth" = {
+        "format" = " {status}";
+        "format-disabled" = "";
+        "format-connected" = " {num_connections}";
+        "tooltip-format" = "{device_alias}";
+        "tooltip-format-connected" = " {device_enumerate}";
+        "tooltip-format-enumerate-connected" = "{device_alias}";
       };
     };
     style = builtins.readFile "/etc/nixos/kurnousal-nixos/home-manager/waybar/theme.css";
