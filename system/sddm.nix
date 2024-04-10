b2220a808e79bc7cd6e7a7eacc91e224a84ab8e5
SeamusFD <armstrongmediahome@icloud.com>
2024-04-10 12:10:21 -0500

    250 current  2024-04-10 12:10:15  24.05pre609462.4cba8b53da47  6.6.25                          *

--- system/sddm.nix
+++ system/sddm.nix
@@ -3,15 +3,25 @@
   pkgs,
   ...
 }: {
+  services.xserver.displayManager = {
+    session = [
+      {
+        manage = "desktop";
+        name = "hyprland";
+        start = "Hyprland";
+      }
+    ];
+  };
   services.xserver.displayManager.sddm = {
     enable = true;
     enableHidpi = true;
     wayland = {
       enable = true;
+      compositorCommand = "Hyprland";
     };
     settings = {
       AutoLogin = {
-        Session = "Hyprland";
+        Session = "hyprland.desktop";
         User = "benjaminc";
       };
     };
