ef10f7f064098f7146ae481141b3629c33836605
SeamusFD <armstrongmediahome@icloud.com>
2024-05-02 14:08:49 -0500

    292 current  2024-05-02 13:13:48  24.05pre617818.58a1abdbae32  6.6.28                          *

--- home-manager/rofi/rofi.nix
+++ home-manager/rofi/rofi.nix
@@ -5,6 +5,7 @@
 }: {
   programs.rofi = {
     enable = true;
+    configPath = "$XDG_CONFIG_HOME/rofi/config.rasi";
     theme = "/etc/nixos/kurnousal-nixos/home-manager/rofi/themes/Catppuccin-Mocha.rasi";
     font = "CaskaydiaCove NerdFont";
     plugins = with pkgs; [
