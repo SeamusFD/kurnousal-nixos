d61fd8f333046341b23915d1f8fd0817dfc97baf
SeamusFD <armstrongmediahome@icloud.com>
2024-05-02 13:59:34 -0500

    292 current  2024-05-02 13:13:48  24.05pre617818.58a1abdbae32  6.6.28                          *

--- home-manager/rofi/rofi.nix
+++ home-manager/rofi/rofi.nix
@@ -6,5 +6,13 @@
   programs.rofi = {
     enable = true;
     theme = "/etc/nixos/kurnousal-nixos/home-manager/rofi/themes/Catppuccin-Mocha.rasi";
+    font = "CaskaydiaCove NerdFont";
+    plugins = with pkgs; [
+      rofimoji
+      rofi-calc
+      rofi-rbw
+      rofi-vpn
+      rofi-systemd
+    ];
   };
 }
