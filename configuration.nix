af9a9c168b89fab4763f5600c069795f461a5bae
SeamusFD <armstrongmediahome@icloud.com>
2024-05-07 15:13:38 -0500

    311 current  2024-05-07 15:13:35  24.05pre619660.63c3a29ca824  6.6.29                          *

--- configuration.nix
+++ configuration.nix
@@ -18,6 +18,27 @@
     ./scripts/custom-bash-scripts.nix
   ];
 
+  # Security
+  security.sudo = {
+    enable = true;
+    extraRules = [
+      {
+        commands = [
+          {
+            command = "${pkgs.systemd}/bin/nix-rebuild";
+            options = ["NOPASSWD"];
+          }
+        ];
+        groups = ["wheel"];
+      }
+    ];
+    extraConfig = with pkgs; ''
+      Defaults:picloud secure_path="${lib.makeBinPath [
+        systemd
+      ]}:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
+    '';
+  };
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
