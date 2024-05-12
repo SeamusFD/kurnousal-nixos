0408bd1df3d5cdc583115f255025bbd2e91bdca2
SeamusFD <armstrongmediahome@icloud.com>
2024-05-12 00:08:08 -0500

    323 current  2024-05-11 21:40:18  24.05pre622847.b211b392b848  6.6.30                          *

---
+++ scripts/hyprland/toggle-blur.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+HYPR_CONFIG="/etc/nixos/kurnousal-nixos/home-manager/hyprland/conf/"
+ACTIVEWINDOW=$(hyprctl activewindow -j | jq -r ".class")
+
+REGEX_LINE=$(grep -n $ACTIVEWINDOW $HYPR_CONFIG/window_rules.conf | cut -d : -f 1)
+
+if [ -n "$REGEX_LINE" ]; then
+	sed -i "${REGEX_LINE}s|\([^0-9]*\)\([^a-z]*\)\([^0-9]*\)\([.0-9]*\)\([^0-9]*\)\([.0-9]*\)|\1\2\3\6\5\4|" $HYPR_CONFIG/window_rules.conf
+else
+	sed -i "6 i windowrulev2 = opacity 1.0,class:${ACTIVEWINDOW} #!0.9" $HYPR_CONFIG/window_rules.conf
+fi
