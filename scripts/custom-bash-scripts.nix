8a839a63f501632e9b1c871922ebb17be3ec56c9
SeamusFD <armstrongmediahome@icloud.com>
2024-05-11 21:40:22 -0500

    323 current  2024-05-11 21:40:18  24.05pre622847.b211b392b848  6.6.30                          *

--- scripts/custom-bash-scripts.nix
+++ scripts/custom-bash-scripts.nix
@@ -1,11 +1,18 @@
 {pkgs, ...}:
 with builtins; let
-  rebuild = pkgs.writeShellScriptBin "nix-rebuild" (builtins.readFile "/etc/nixos/kurnousal-nixos/scripts/nix/nixos-rebuild.sh");
-  set-primary-monitor = pkgs.writeShellScriptBin "set-primary-monitor" (builtins.readFile "/etc/nixos/kurnousal-nixos/scripts/hyprland/primary-monitor.sh");
-  gamemode = pkgs.writeShellScriptBin "gamemode" (builtins.readFile "/etc/nixos/kurnousal-nixos/scripts/hyprland/gamemode.sh");
+  rebuild =
+    pkgs.writeShellScriptBin "nix-rebuild"
+    (builtins.readFile "/etc/nixos/kurnousal-nixos/scripts/nix/nixos-rebuild.sh");
+  set-primary-monitor =
+    pkgs.writeShellScriptBin "set-primary-monitor"
+    (builtins.readFile "/etc/nixos/kurnousal-nixos/scripts/hyprland/primary-monitor.sh");
+  gamemode =
+    pkgs.writeShellScriptBin "gamemode"
+    (builtins.readFile "/etc/nixos/kurnousal-nixos/scripts/hyprland/gamemode.sh");
 in {
   environment.systemPackages = [
     rebuild
     set-primary-monitor
+    gamemode
   ];
 }
