2668b1a6b4b8713e1fe14113e59f046fdcd9d656
SeamusFD <armstrongmediahome@icloud.com>
2024-05-16 11:03:56 -0500

    344 current  2024-05-16 11:03:52  24.05pre625274.205781405197  6.6.30                          *

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -8,6 +8,18 @@ in {
   imports = [
     (import "${home-manager}/nixos")
   ];
+  environment.sessionVariables = {
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    XDG_SESSION_DESKTOP = "Hyprland";
+    XDG_SESSION_TYPE = "wayland";
+    GDK_BACKEND = "wayland";
+    GTK_USE_PORTAL = "1";
+    QT_QPA_PLATFORMTHEME = "qt5ct";
+    QT_QPA_PLATFORM = "wayland";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+  };
   users.users.bcampbell = {
     isNormalUser = true;
     description = "bcampbell";
@@ -118,7 +130,7 @@ in {
 
     qt = {
       enable = true;
-      platformTheme.name = "qtct";
+      platformTheme.name = "qt5ct";
     };
 
     xdg.configFile."Kvantum/kvantum.kvconfig".source = (pkgs.formats.ini {}).generate "kvantum.kvconfig" {
